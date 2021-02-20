d3294eef44c5d27f5fc77e8bd0dbc26cf85c65d8
negirohan <rohan.negi@hirepro.in>
2021-02-20 20:34:29 +0530

    small changes for ui

--- src/app/vendorHome/home.component.js
+++ src/app/vendorHome/home.component.js
@@ -52,7 +52,9 @@
     }
 
     function toggleItem(index) {
+      // vm.myOrders.pendding[selectedIndex].orderItems[index].status = !vm.myOrders.pendding[selectedIndex].orderItems[index].status;
       vm.myOrders.pendding[selectedIndex].packedCount = _.size(_.filter(vm.myOrders.pendding[selectedIndex].orderItems, 'status'));
+      console.log(vm.myOrders);
     }
 
     function selectOrder(index, status) {
